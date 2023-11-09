62085dc7361875d2f426c0a15f06ce5c4285397d
reyes <reyes@DESKTOP-FT2IN39>
2023-11-09 17:01:25 -0600

    Agregar archivos de proyecto.

---
+++ practica20/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace practica20
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Showcard Gothic", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(261, 25);
+            label1.Name = "label1";
+            label1.Size = new Size(242, 23);
+            label1.TabIndex = 0;
+            label1.Text = " Verificador de edad";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(169, 147);
+            label2.Name = "label2";
+            label2.Size = new Size(118, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Ingresa tu edad";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(315, 147);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(98, 23);
+            textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(205, 200);
+            button1.Name = "button1";
+            button1.Size = new Size(307, 74);
+            button1.TabIndex = 3;
+            button1.Text = "Revisar";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(192, 255, 255);
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private Button button1;
+    }
+}
---
+++ practica20/Form1.cs
@@ -0,0 +1,23 @@
+using System.Diagnostics.Eventing.Reader;
+
+namespace practica20
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int edad;
+            edad= int.Parse(textBox1.Text);
+            if (edad > 0 && edad < 11) MessageBox.Show("Eres un niño");
+            else if (edad > 12 && edad < 17) MessageBox.Show("Eres un adolescente");
+            else if (edad > 18 && edad < 60) MessageBox.Show("Eres un Adulto");
+            else if (edad >=68) MessageBox.Show("Eres un Adulto Mayor");
+
+        }
+    }
+}
